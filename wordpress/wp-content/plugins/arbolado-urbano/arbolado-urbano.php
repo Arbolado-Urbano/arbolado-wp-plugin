3c881a59cb74afd6a91c149118feb37cf5becaca
Fermín Ares <fermin@ares.uy>
2024-11-29 10:17:13 -0300

    Add WP coding standards

--- wordpress/wp-content/plugins/arbolado-urbano/arbolado-urbano.php
+++ wordpress/wp-content/plugins/arbolado-urbano/arbolado-urbano.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * @package Arbolado Urbano
+ * Arbolado Urbano
+ * php version 8.3
+ *
+ * @category Arbolado-Urbano
+ * @package  Arbolado-Urbano/arbolado-wp-plugin
+ * @author   Fermín Ares <fermin@ares.uy>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @version  GIT: 1.0.0
+ * @link     https://github.com/Arbolado-Urbano/arbolado-wp-plugin/
  */
+
 /*
 Plugin Name: Arbolado Urbano
 Plugin URI: https://github.com/Arbolado-Urbano/arbolado-wp-plugin/
@@ -11,3 +20,5 @@ Author: Fermín Ares
 Author URI: https://ares.uy/
 Text Domain: arbolado-urbano
 */
+
+add_shortcode( "string $tag", "callable $callback" );
